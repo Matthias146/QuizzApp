95bee051b3548aa160210c8256c69539968fe919
Matthias Hammelehle <mahammelehle@outlook.de>
2024-02-14 06:05:28 +0100

    adde question Json

---
+++ script.js
@@ -0,0 +1,42 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    },
+    {
+        "question": "Was bedeutet das HTML Tag a?",
+        "answer_1": "Text Fett",
+        "answer_2": "Container",
+        "answer_3": "Ein Link",
+        "answer_4": "Kursiv",
+        "right_answer": 3
+    },
+    {
+        "question": "Wie beginnt man ein Html Dokument",
+        "answer_1": "<HTML>",
+        "answer_2": "<HTML Start>",
+        "answer_3": "<HTML GO>",
+        "answer_4": "<!DOCTYPE>",
+        "right_answer": 4
+    },
+    {
+        "question": "Was befindet sich im Tag <img src='...' />?",
+        "answer_1": "Ein Video...",
+        "answer_2": "Eine Datei...",
+        "answer_3": "Ein Text...",
+        "answer_4": "Ein Bild...",
+        "right_answer": 4
+    },
+    {
+        "question": "Was befindet sich meistens innerhalb des <section> Tags?",
+        "answer_1": "Der Body Bereich... <body>",
+        "answer_2": "Ein Artikel... <article>",
+        "answer_3": "Der Head Bereich... <head>",
+        "answer_4": "Die Navigation... <nav>",
+        "right_answer": 2
+    },
+]
